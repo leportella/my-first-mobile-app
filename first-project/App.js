746d10d551b90b20c76ec8312f2009aab2c8695a
Leticia Portella <leportella@gmail.com>
2018-04-17 10:02:23 -0300

    Add small welcome text and style on App.js

--- first-project/App.js
+++ first-project/App.js
@@ -5,9 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.welcome}>
+         Welcome to React Native!
+        </Text>
       </View>
     );
   }
@@ -19,5 +19,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
