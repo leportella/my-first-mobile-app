cfa89ffd3cf12e7a80bc97d078712364261eebb1
Leticia Portella <leportella@gmail.com>
2018-04-17 13:14:25 -0300

    Added TextInput and state changes

--- first-project/WeatherProject.js
+++ first-project/WeatherProject.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default class WeatherProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { zip: "" };
+  }
+
+  _handlerTextChange = event => {
+    this.setState({zip: event.nativeEvent.text})
+  }
+
   render () {
     return (
       <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onSubmitEditing={this._handlerTextChange}/>
         <Text style={styles.welcome}>
-          Welcome to my first app!
+          You input { this.state.zip }
         </Text>
       </View>
     );
@@ -20,6 +33,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input : {
+    fontSize: 20,
+    borderWidth: 2,
+    height: 40,
   },
   welcome: {
     fontSize: 20,
