7bda17d1c600f482a8bad2853986e9085520a627
Leticia Portella <leportella@gmail.com>
2018-04-17 16:41:27 -0300

    Added api call and Forecast Component

--- first-project/src/weather/WeatherProject.js
+++ first-project/src/weather/WeatherProject.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
+import Forecast from './Forecast';
 import OpenWeatherMap from './open_weather_map';
 
 
 class WeatherProject extends Component {
   constructor(props) {
     super(props);
-    this.state = { zip: "" };
+    this.state = { zip: "", forecast: null };
   }
 
   _handlerTextChange = event => {
-    this.setState({zip: event.nativeEvent.text});
+    let zip = event.nativeEvent.text;
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      this.setState({ forecast: forecast });
+    });
+    this.setState({zip: zip});
   }
 
   render () {
+    let content = null;
+    if (this.state.forecast !== null) {
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp}
+        />
+      )
+    }
+
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           You input {this.state.zip}
         </Text>
+        {content}
         <TextInput
           style={styles.input}
           onSubmitEditing={this._handlerTextChange}/>
